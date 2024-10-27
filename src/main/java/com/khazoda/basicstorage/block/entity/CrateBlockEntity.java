5c8c51b3170e156924361aa6b86f43c51f8be5e0
June <khazoda@gmail.com>
2024-10-27 10:57:17 +0000

    supress unstable transaction api warnings

--- src/main/java/com/khazoda/basicstorage/block/entity/CrateBlockEntity.java
+++ src/main/java/com/khazoda/basicstorage/block/entity/CrateBlockEntity.java
@@ -12,6 +12,7 @@ import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
+@SuppressWarnings("UnstableApiUsage")
 public class CrateBlockEntity extends BlockEntity {
   public final CrateSlot storage = new CrateSlot(this);
 
